f13beb388ccea6f38899d9c146c7d19f17224852
Adam Daley <admdly@users.noreply.github.com>
2025-05-31 00:39:17 +0100

    Add Central Alerts API (v1/legacy)

---
+++ src/central-alerts/v1/interfaces.ts
@@ -0,0 +1,16 @@
+export type CentralAlert = {
+    id: string;
+    title: string;
+    message: string;
+    type: 'success' | 'info' | 'warning' | 'danger';
+    dismissible: boolean;
+    min_fossbilling_version: string;
+    max_fossbilling_version: string;
+    include_preview_branch: boolean;
+    buttons?: {
+        text: string;
+        link: string;
+        type?: 'success' | 'info' | 'warning' | 'danger';
+    }[];
+    datetime: string; // Use the ISO 8601 format. The easies way is to just open a browser and type "new Date().toISOString()" in the console.
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Hono } from "hono";
+import centralAlertsV1 from './central-alerts/v1';
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
+
+// Central Alerts Service
+app.route("/central-alerts/v1", centralAlertsV1);
 
 export default app;
